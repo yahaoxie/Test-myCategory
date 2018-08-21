ab032388f7dac6faf33045deb8f435dc3a3b1a17
xieyahao <1498742645@qq.com>
2018-08-21 16:05:00 +0800

    Initial Commit

---
+++ Test-myCategory/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Test-myCategory
+//
+//  Created by xyh on 2018/8/21.
+//  Copyright © 2018年 xyh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Test-myCategoryTests/Test_myCategoryTests.m
@@ -0,0 +1,39 @@
+//
+//  Test_myCategoryTests.m
+//  Test-myCategoryTests
+//
+//  Created by xyh on 2018/8/21.
+//  Copyright © 2018年 xyh. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Test_myCategoryTests : XCTestCase
+
+@end
+
+@implementation Test_myCategoryTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
